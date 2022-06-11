a3e4a81aa2dafaff75d29e96351f1b5cb436722e
Nikolas Furtado <nikolasfurtado@gmail.com>
2022-06-11 00:13:35 -0400

    css almost ready

---
+++ datestate.js
@@ -0,0 +1,9 @@
+var day = document.getElementsByClassName("showday");
+var week = document.getElementsByClassName("showweek");
+var month = document.getElementsByClassName("showmonth");
+
+function changecards(params) {
+
+
+
+}
