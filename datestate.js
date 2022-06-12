07b9bcb03ca804b167531dde82aafa3aef546b2c
Nikolas Furtado <nikolasfurtado@gmail.com>
2022-06-12 19:22:31 -0400

    css almost ready

--- datestate.js
+++ datestate.js
@@ -1,9 +1,40 @@
-var day = document.getElementsByClassName("showday");
-var week = document.getElementsByClassName("showweek");
-var month = document.getElementsByClassName("showmonth");
+function showresult() {
 
-function changecards(params) {
+    const day = document.querySelectorAll('.dayshow');
 
 
+    day.forEach(element => {
+        element.textContent = 'display:none';
 
+    });
+    /*
+
+        const day = document.querySelector('.dayshow');
+
+        day.classList.replace('dayshow', 'hide');
+    */
 }
+
+
+/*
+
+   test1.innerHTML("fuckinworking");
+
+day.forEach(dayshow => {
+    day.style.display = 'none';
+});
+
+var day = document.querySelectorAll(".dayshow");
+day.forEach(showday);
+
+function showday() {
+    day.innerHTML("ola")
+}
+    day.forEach(dayshow => {
+        day.style.display = 'none';
+    });
+
+    var sv = document.getElementById("secondscreen");
+    day.style.display = 'none';
+    sv.style.display = 'grid';
+*/
